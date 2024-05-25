d706a4d078d6cd4de1fd005952e504450aae1e11
Everesh <everesh@proton.me>
2024-05-25 17:36:22 +0200

    Add some insert testcases

--- test/test.rb
+++ test/test.rb
@@ -2,3 +2,8 @@ require_relative '../lib/binary_search_tree'
 
 tree = BinarySeachTree.new([1,1,1,5,12,7,15,98,48791,15,846,443,58,20])
 tree.print
+
+tree.insert(50000)
+tree.print
+tree.insert(30)
+tree.print
