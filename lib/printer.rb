d6c68bc33cb3d9d92d09da78f8857a38ddb538b0
Everesh <everesh@proton.me>
2024-05-25 17:18:39 +0200

    Rename bst.rb and fix printer

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,7 +1,7 @@
 module Printer
   def print(node = @root, prefix = '', is_left = true)
     print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.val}"
     print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
